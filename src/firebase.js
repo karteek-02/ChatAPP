51b80c7663f0c3e7646f9af2e661c78efc674b40
karteek-02 <karteekkarteek007@gmail.com>
2023-08-07 15:20:56 +0530

    Last Commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-// require('dotenv').config();
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "chat-app-5efa9.firebaseapp.com",
-  projectId: "chat-app-5efa9",
-  storageBucket: "chat-app-5efa9.appspot.com",
-  messagingSenderId: "628424508635",
-  appId: "1:628424508635:web:3c558405b732037f09b8fe"
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID
 };
 
 // Initialize Firebase
