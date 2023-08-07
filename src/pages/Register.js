e0f7fddd8eb6aa9f986142c49fb787f5b12ee78e
karteek-02 <karteekkarteek007@gmail.com>
2023-08-07 15:22:30 +0530

    Last Commit

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -62,7 +62,7 @@ const Register = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
+        <span className="logo">Chat APP</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="Display Name" />
